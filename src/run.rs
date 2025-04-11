292c95a6ee2a8ff5cfdb4589b992b57b042629a2
Andrea Berlingieri <andrea-berling@users.noreply.github.com>
2025-04-11 12:23:26 +0200

    Add support for expression statement and runtime errors

--- src/run.rs
+++ src/run.rs
@@ -3,22 +3,48 @@ use crate::{
     parser::{Program, Statement},
 };
 
-pub fn run_statement(statement: &Statement) {
+#[derive(Debug)]
+pub enum RuntimeErrorType {
+    EvalError,
+}
+
+#[derive(Debug)]
+pub struct RuntimeError {
+    pub line: usize,
+    pub error: RuntimeErrorType,
+}
+
+impl RuntimeError {
+    pub fn new(line: usize, error: RuntimeErrorType) -> Self {
+        Self { line, error }
+    }
+}
+
+pub fn run_statement(statement: &Statement) -> Result<(), RuntimeError> {
     match statement {
-        Statement::Expr(expr) => todo!(),
+        Statement::Expr(expr) => {
+            if let Err(err) = eval_expr(expr) {
+                eprintln!("{}\n[line {}]", err.error, err.line);
+                return Err(RuntimeError::new(err.line, RuntimeErrorType::EvalError));
+            }
+            Ok(())
+        }
         Statement::Print(expr) => match eval_expr(expr) {
             Err(EvalError { line, error }) => {
-                eprintln!("[line {}] Error: {}", line, error);
+                eprintln!("{}\n[line {}]", error, line);
+                Err(RuntimeError::new(line, RuntimeErrorType::EvalError))
             }
             Ok(result) => {
                 println!("{}", result);
+                Ok(())
             }
         },
     }
 }
 
-pub fn run_program(program: &Program) {
+pub fn run_program(program: &Program) -> Result<(), RuntimeError> {
     for statement in program.iter() {
-        run_statement(statement);
+        run_statement(statement)?;
     }
+    Ok(())
 }
